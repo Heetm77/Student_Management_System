cd74a16c87875a81eac652271e9e0870a0e8c8cf
Heet <heetmehta2611@gmail.com>
2025-03-17 11:35:28 +0530

    Day 46: Inserting New Database Records

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 # Accessing the module QtWidgets from the PyQt6 library and the module has
 # several classes like QApplication, QVBoxLayout
+from tkinter.font import names
+
 from PyQt6.QtWidgets import (QApplication, QLabel, QWidget,
-                             QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem)
+                             QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, QDialog,
+                             QVBoxLayout, QComboBox)
 from PyQt6.QtGui import QAction
 import sys
 import sqlite3
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -14,7 +18,10 @@ class MainWindow(QMainWindow):
         file_menu_item = self.menuBar().addMenu("&File")
         help_menu_item = self.menuBar().addMenu("&Help")
 
+        # Add student is a sub menu item
         add_student_action = QAction("Add Student", self)
+        # triggered is the property of QAction instance
+        add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
         about_action = QAction("About", self)
@@ -36,11 +43,65 @@ class MainWindow(QMainWindow):
                 self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
         connection.close()
 
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.exec()
+
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        # self refers to the InsertDialog Instance
+        self.setWindowTitle("Insert Student Data")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        # Create the widgets
+        # Add student name widget
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
+
+        # Add combo box of courses
+        self.course_name = QComboBox()
+        courses = ["Biology", "Math", "Astronomy", "Physics"]
+        self.course_name.addItems(courses)
+        layout.addWidget(self.course_name)
+
+        # Add mobile widget
+        self.mobile_number = QLineEdit()
+        self.mobile_number.setPlaceholderText("Mobile")
+        layout.addWidget(self.mobile_number)
+
+        # Add a submit button
+        button = QPushButton("Register")
+        button.clicked.connect(self.add_student)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+    def add_student(self):
+        # extract the text using the text method
+        name = self.student_name.text()
+        # course_name is a combo box, so we need to use itemText method to extract the text
+        course = self.course_name.itemText(self.course_name.currentIndex())
+        mobile = self.mobile_number.text()
+        connection = sqlite3.connect("database.db")
+        # Since we are inserting data, we need a cursor object
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO students (name, course, mobile) VALUES (?, ?, ?)",
+                       (name, course, mobile))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        main_window.load_data()
 
 
 app = QApplication(sys.argv)
 # Constructing QWidget AgeCalculator which is a child of QWidget
-age_calculator = MainWindow()
-age_calculator.show()
-age_calculator.load_data()
+main_window = MainWindow()
+main_window.show()
+main_window.load_data()
 sys.exit(app.exec())
