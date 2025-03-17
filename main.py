a15126308384e55c08f91a73b202ddeb29aada29
Heet <heetmehta2611@gmail.com>
2025-03-17 16:37:11 +0530

    Day 47: Add a statusbar

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from tkinter.font import names
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (QApplication, QLabel, QWidget,
                              QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, QDialog,
-                             QVBoxLayout, QComboBox, QToolBar)
+                             QVBoxLayout, QComboBox, QToolBar, QStatusBar)
 from PyQt6.QtGui import QAction, QIcon
 import sys
 import sqlite3
@@ -50,6 +50,29 @@ class MainWindow(QMainWindow):
         toolbar.addAction(add_student_action)
         toolbar.addAction(add_search_action)
 
+        # Create status bar and add status bar elements
+        self.statusbar = QStatusBar()
+        self.setStatusBar(self.statusbar)
+
+        # Detect a cell click
+        # cellClicked is a property of table and connect is a method of cellClicked property
+        self.table.cellClicked.connect(self.cell_clicked)
+
+    def cell_clicked(self):
+        edit_button = QPushButton("Edit Record")
+        edit_button.clicked.connect(self.edit)
+
+        delete_button = QPushButton("Delete Record")
+        delete_button.clicked.connect(self.delete)
+
+        children = self.findChildren(QPushButton)
+        if children:
+            for child in children:
+                self.statusbar.removeWidget(child)
+
+        self.statusbar.addWidget(edit_button)
+        self.statusbar.addWidget(delete_button)
+
     def load_data(self):
         connection = sqlite3.connect("database.db")
         result = connection.execute("SELECT * FROM students")
@@ -67,6 +90,23 @@ class MainWindow(QMainWindow):
     def search_student(self):
         dialog = SearchDialog()
         dialog.exec()
+
+    def edit(self):
+        dialog = EditDialog()
+        dialog.exec()
+
+    def delete(self):
+        dialog = DeleteDialog()
+        dialog.exec()
+
+
+class EditDialog(QDialog):
+    pass
+
+
+class DeleteDialog(QDialog):
+    pass
+
 
 class InsertDialog(QDialog):
     def __init__(self):
