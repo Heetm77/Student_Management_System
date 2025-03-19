936eb7a21404b5f7e3461597ef93a1357890e7f6
Heet <heetmehta2611@gmail.com>
2025-03-19 10:55:47 +0530

    Day 47: Create an About Dialog

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Accessing the module QtWidgets from the PyQt6 library and the module has
 # several classes like QApplication, QVBoxLayout
-import traceback
+
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (QApplication, QLabel, QWidget,
                              QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, QDialog,
@@ -27,6 +27,7 @@ class MainWindow(QMainWindow):
 
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
+        about_action.triggered.connect(self.about)
 
         # Add search is a sub menu item of Edit menu item
         add_search_action = QAction(QIcon("icons/icons/search.png"),"Search", self)
@@ -95,6 +96,21 @@ class MainWindow(QMainWindow):
     def delete(self):
         dialog = DeleteDialog()
         dialog.exec()
+
+    def about(self):
+        dialog = AboutDialog()
+        dialog.exec()
+
+
+class AboutDialog(QMessageBox):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("About")
+        content = """
+        This app was created during the course "The Python Mega Course".
+        Feel free to modify and reuse this app.
+        """
+        self.setText(content)
 
 
 class EditDialog(QDialog):
